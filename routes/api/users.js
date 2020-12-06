3c7073bf830479069195da2bf4e576a989f85f5a
gxc <gxc5623@sina.cn>
2020-12-06 10:12:57 +0800

    aa

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,8 +12,8 @@ const passport = require("passport");
 // $route  GET api/users/test
 // @desc   返回的请求的json数据
 // @access public
-router.get('/test',(req,res) => {
-    res.json({msg:"logon users test"})
+router.get('/test', (req, res) => {
+    res.json({ msg: "logon users test" })
 })
 
 // $route  POST /api/users/register
@@ -23,7 +23,7 @@ router.post('/register', (req, res) => {
     // 查询数据库中是否拥有邮箱
     User.findOne({ email: req.body.email }).then(user => {
         if (user) {
-            return res.status(400).json( '邮箱已被注册!');
+            return res.status(400).json('邮箱已被注册!');
         } else {
             const avatar = gravatar.url(req.body.email, {
                 s: '200',
@@ -39,7 +39,7 @@ router.post('/register', (req, res) => {
                 identity: req.body.identity
             });
 
-            bcrypt.genSalt(10, function (err, salt) {
+            bcrypt.genSalt(10, function(err, salt) {
                 bcrypt.hash(newUser.password, salt, (err, hash) => {
                     if (err) throw err;
 
@@ -65,27 +65,27 @@ router.post('/login', (req, res) => {
     const password = req.body.password;
 
     //查询数据库
-    User.findOne({email:req.body.email}).then(user => {
+    User.findOne({ email: req.body.email }).then(user => {
         //判断用户是否存在
-        if(!user){
+        if (!user) {
             return res.status(404).json("用户不存在")
         }
         //密码匹配
-        bcrypt.compare(password, user.password).then(isMath =>{
-            if(isMath) {
+        bcrypt.compare(password, user.password).then(isMath => {
+            if (isMath) {
                 const rule = {
-                    id: user.id, 
-                    name: user.name, 
+                    id: user.id,
+                    name: user.name,
                     avatar: user.avatar,
                     identity: user.identity
-                }; 
-                jwt.sign(rule, keys.secretOrKey,{expiresIn:3600},(err,token) =>{
-                    if(err) throw err;
+                };
+                jwt.sign(rule, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                    if (err) throw err;
                     res.json({
-                        success:true,
-                        token: "Bearer "+token   //最后返回token值
+                        success: true,
+                        token: "Bearer " + token   //最后返回token值
                     })
-               })
+                })
             } else {
                 return res.status(400).json("密码不存在")
             }
@@ -94,10 +94,10 @@ router.post('/login', (req, res) => {
 })
 
 // $route  GET /api/users/current
-// @desc   返回用户信息
+// @desc   返回用户信息  c
 // @access private
 
-router.get('/current',passport.authenticate('jwt', { session: false }),
+router.get('/current', passport.authenticate('jwt', { session: false }),
     (req, res) => {
         res.json({
             id: req.user.id,
